054543f383e0cde116a4ab2d21912750c689a8c1
Matmonsen <sindre.nerdal@gmail.com>
2016-02-26 02:29:55 +0100

    Added ajax calls. Gjorde ferdig ui

--- SemesterOppgave/WebApp/Content/js/site.js
+++ SemesterOppgave/WebApp/Content/js/site.js
@@ -1,74 +1,214 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
-    // Event listeners
-    document.getElementById("sign-in-button").addEventListener("click", ShowSignIn);
-    document.getElementById("sign-up-button").addEventListener("click", ShowSignUp);
-    document.addEventListener("keydown", EscClicked);
-
-    // Global variables
-    var selectedRow = null;
-    var reservationJson = null;
-    var highlightRowColor = "#4f94cf";
-    var defaultRowColor = "white";
-    var errorColor = "red";
-    var defaultColor = "black";
+    /* ================================================================================================================
+                                                     Global variables
+       ================================================================================================================ */
+    // Modals
     var singInModal = document.getElementById("sign-in-modal");
     var singUpModal = document.getElementById("sign-up-modal");
     var reservationModal = document.getElementById("reservation-modal");
+    // Buttons
+    var signInModalButton = document.getElementById("sign-in-modal-button");
+    var signUpnModalButton = document.getElementById("sign-up-modal-button");
+    var signOutButton = document.getElementById("logout-button");
+    // Dropdowns
+    var qualityDropDown = document.getElementById("roomQuality");
+    var sizeDropDown = document.getElementById("roomSize");
+    var bedsDropDown = document.getElementById("roomBeds");
+    // Textfields
+    var startTextField = document.getElementById("startTextField");
+    var endTextField = document.getElementById("endTextField");
+    // Table
+    var table = document.getElementById("resultTable");
+    var selectedRow = null;
+    var highlightRowColor = "#4f94cf";
+    var defaultRowColor = "white";
+    // Cached
+    var startDate;
+    var endDate;
+    var email;
+    var ticket;
+    // HttpRequests
+    var baseUrl = "http://localhost:3274/Ajax/";
+    var xhr;
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
 
+
+    /* ================================================================================================================
+                                                 Event listeners
+       ================================================================================================================ */
+    document.getElementById("search-button").addEventListener("click", search);
+    document.getElementById("reservation-button").addEventListener("click", makeReservation);
+    signInModalButton.addEventListener("click", showSignIn);
+    signUpnModalButton.addEventListener("click", showSignUp);
+    //signOutButton.addEventListener("click", signOut);
+    document.getElementById("email-button").addEventListener("click", reset);
+    document.getElementById("sign-in-to-sign-up").addEventListener("click", showSignUp);
+    document.getElementById("sign-up-to-sign-in").addEventListener("click", showSignIn);
+    document.getElementById("sign-in-button").addEventListener("click", signIn);
+    document.getElementById("sign-up-button").addEventListener("click", signUp);
+    document.addEventListener("keydown", escClicked);
+
+    /* ================================================================================================================
+                                                     Main functions 
+       ================================================================================================================ */
+
+    // Resets search filters and deletes all element from table
+    function reset() {
+        closeModals();
+        table.removeChild(table.getElementsByTagName("tbody")[0]);
+        startTextField.value = "";
+        endTextField.value = "";
+        qualityDropDown.selectedIndex = 0;
+        sizeDropDown.selectedIndex = 0;
+        bedsDropDown.selectedIndex = 0;
+    }
+
+    // Hides sign ins, and shows logout.
+    function trySignIn(data) {       
+        var json = JSON.parse(JSON.parse(data.responseText));
+        if (json.Success) {
+            signUpnModalButton.style.display = "none";
+            signOutButton.style.display = "inline-block";
+            signOutButton.value = signOutButton.value + " " + json.FirstName + " "+ json.LastName;
+            signInModalButton.style.display = "none";
+            ticket = json.Ticket;
+            closeModals();
+        } else {
+            showSignIn();
+        }
+    }
+
+    // Sign up click event
+    function signUp() {
+        email = document.getElementById("signUpEmail").value;
+        var password = document.getElementById("signUpPassword").value;
+        var lastname = document.getElementById("signUpLastname").value;
+        var firstname = document.getElementById("signUpFirstname").value;
+        ajax("POST", baseUrl + "signup", JSON.stringify({ "Email": email, "Password": password, "FirstName": firstname, "LastName": lastname }), trySignIn, ajaxError);
+    }
+
+    // Sign in click event
+    function signIn() {
+        email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+        ajax("POST", baseUrl + "signin", JSON.stringify({ "Email": email, "Password": password }), trySignIn, ajaxError);
+    }
+
+    // search click event
+    function search() {
+        startDate = startTextField.value;
+        endDate = endTextField.value;
+        var searchParamater = "/?Quality=" + qualityDropDown.options[qualityDropDown.selectedIndex].value +
+                                "&Size=" + sizeDropDown.options[sizeDropDown.selectedIndex].value +
+                                "&Beds=" + bedsDropDown.options[bedsDropDown.selectedIndex].value +
+                                "&Start=" + startDate +
+                                "&End=" + endDate;
+        table.removeChild(table.getElementsByTagName("tbody")[0]);
+        ajax("GET", baseUrl + "Search" + searchParamater, null, populateTable, ajaxError);
+    }
+
+    // make reservation click event
+    function makeReservation() {
+
+        if (selectedRow == null) {
+            alert("Select a row first!");
+            return;
+        }
+   
+        var reservationJson = {
+                "Start": startDate,
+                "End": endDate,
+                "Quality": selectedRow[0].innerHTML,
+                "Size": selectedRow[1].innerHTML,
+                "Beds": selectedRow[2].innerHTML,
+                "Email": email
+        }
+        
+        ajax("POST", baseUrl + "makeReservation", JSON.stringify(reservationJson),
+            function (data) {
+                var reservation = JSON.parse(JSON.parse(data.responseText));
+                
+                document.getElementById("reservationFirstname").value = reservation.Customer.FirstName;
+                document.getElementById("reservationLastname").value = reservation.Customer.LastName;
+                document.getElementById("reservationEmail").value = reservation.Customer.Email;
+                document.getElementById("reservationStart").value = reservation.Start.split("T")[0];
+                document.getElementById("reservationEnd").value = reservation.Slutt.split("T")[0];
+                document.getElementById("reservationSize").value = reservation.Room.Size.Size;
+                document.getElementById("reservationQuality").value = reservation.Room.Quality.Quality;
+                document.getElementById("reservationBeds").value = reservation.Room.Beds.Beds;
+                //document.getElementById("reservationPrice").value = reservation.Price;
+
+                showReservation();
+
+        },ajaxError);
+    }
+
+
+    /* ================================================================================================================
+                                                         UI functions 
+       ================================================================================================================ */
     // Shows Sign in modal
-    function ShowSignIn() {
-        CloseModals();
+    function showSignIn() {
+        closeModals();
         singInModal.style.display = "inline-block";
     }
 
     // Shows sign up modal
-    function ShowSignUp() {
-        CloseModals();
+    function showSignUp() {
+        closeModals();
         singUpModal.style.display = "inline-block";
     }
 
     // Shows sign up modal
-    function ShowReservation() {
-        CloseModals();
+    function showReservation() {
+        closeModals();
         reservationModal.style.display = "inline-block";
     }
 
     // When the user clicks anywhere outside of the modal, close it
-    function EscClicked (e) {
-        if (e.keyCode == 27)
-            CloseModals();
+    function escClicked(e) {
+        if (e.keyCode === 27) {
+            closeModals();
+        }
     }
 
     // Closes modals
-    function CloseModals() {
+    function closeModals() {
         singUpModal.style.display = "none";
         singInModal.style.display = "none";
         reservationModal.style.display = "none";
     }
 
+    /* ================================================================================================================
+                                                     Helper functions 
+       ================================================================================================================ */
+
     // Add click functions to table rows
-    function addRowHandlers() {
-        var rows = document.getElementById("resultTable").getElementsByTagName("tr");
-        for (i = 0; i < rows.length; i++) {
+    function setRowClickHandlers() {
+        var rows = table.getElementsByTagName("tr");
+        for (var i = 0; i < rows.length; i++) {
             var currentRow = rows[i];
             
-            currentRow.onclick = createClickHandler(currentRow);
+            currentRow.onclick = createClickHandlerForRow(currentRow);
         }
     }
 
     // Click handler for a row
-    var createClickHandler = function (row) {
+    function createClickHandlerForRow(row) {
         return function () {
             try {
                 var cells = row.getElementsByTagName("td");
                 if (cells == null)
                     return;
 
-
                 if (selectedRow != null)
                     colorRow(selectedRow, defaultRowColor);
 
-                if (selectedRow == cells)
+                if (selectedRow === cells)
                     colorRow(selectedRow, defaultRowColor);
                 else
                     colorRow(cells, highlightRowColor);
@@ -79,44 +219,57 @@
         };
     };
 
-    function reserve() {
-
-        // bruk selected row for å hente data
-
-        // error handling
-        document.getElementById("start").value == "needs a value";
-        document.getElementById("start").style.color = errorColor;
-        document.getElementById("end").value == "needs a value";
-        document.getElementById("end").style.color = errorColor;
-        // Hvis ikke fylt ut tid
-        if (document.getElementById("start").value == "" ) {
-            document.getElementById("start").style.color = errorColor;
-            return;
-        } else if(document.getElementById("end").value == "") {
-            document.getElementById("end").style.color = errorColor;
-        }
-
-        // Ingen error lengre
-        if (document.getElementById("start").style.color == errorColor ) {
-            document.getElementById("start").style.color = defaultColor;
-        } else if(document.getElementById("end").style.color == errorColor ) {
-            document.getElementById("end").style.color = defaultColor;
-        }
-
-
-        reservationJson = {
-            "quality": cells[0].innerHTML,
-            "size": cells[1].innerHTML,
-            "beds": cells[2].innerHTML,
-            "start": document.getElementById("start"),
-            "end": document.getElementById("end")
-        }
-    }
-
+    // Colors a table row
     function colorRow(list, color) {
         for (var i = 0; i < list.length; i++)
             list[i].style.background = color;
     }
 
-    addRowHandlers();
+    // General ajax call
+    function ajax(method, url, data, callbackOk, callbackError) {
+        xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(data);
+        
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) 
+                callbackOk(xhr);
+            else 
+                callbackError();
+        }
+    };
+
+    // Adds a cell to a row
+    function addRowToTable(row, cell, value) {
+        var newCell = row.insertCell(cell);
+        var newText = document.createTextNode(value);
+        newCell.appendChild(newText);
+    }
+
+    // Populates the table with data
+    function populateTable(data) {
+        var newTbody = document.createElement('tbody');      
+        var jsonArray = JSON.parse(JSON.parse(data.responseText));
+
+        if(jsonArray == undefined)
+            return;
+
+        // Adds a new row to the table and populate it
+        for (var i = 0, len = jsonArray.length; i < len; i++) {
+
+            var newRow = newTbody.insertRow(newTbody.rows.length);
+            addRowToTable(newRow, 0, jsonArray[i].Quality.Quality);
+            addRowToTable(newRow, 1, jsonArray[i].Size.Size);
+            addRowToTable(newRow, 2, jsonArray[i].Beds.Beds);
+        }
+        table.appendChild(newTbody);
+
+        setRowClickHandlers();
+   }
+
+    // Handles ajax errors
+    function ajaxError() {
+    }
+
 }, false);
---
+++ SemesterOppgave/WebApp/Controllers/AjaxController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using HttpRequest;
+using HttpRequest.Models;
+using WebApp.Models;
+using WebAPI.Models;
+using Customer = HttpRequest.Models.Customer;
+using MakeReservation = WebAPI.Models.MakeReservation;
+using Reservation = HttpRequest.Models.Reservation;
+using Room = HttpRequest.Models.Room;
+
+namespace WebApp.Controllers
+{
+    public class AjaxController : Controller {
+        // GET: Ajax
+        [HttpGet]
+        public async Task<ActionResult> Search() {
+            //"/?Quality=0&Size=0&Beds=0&Start=0&End=0";
+            var quality = HttpContext.Request.Params.Get("Quality");
+            var size = HttpContext.Request.Params.Get("Size");
+            var beds = HttpContext.Request.Params.Get("Beds");
+            var start = HttpContext.Request.Params.Get("Start");
+            var end = HttpContext.Request.Params.Get("End");
+
+            var jsonFromWebService =
+                await ApiRequests.Get(ApiUrl.ROOMS, quality + "/" + size + "/" + beds + "/" + start + "/" + end);
+            var deserialized = JsonSerializer<Room>.DeSerializeAsList(jsonFromWebService);
+            var serialized = JsonSerializer<Room>.SerializeList(deserialized);
+
+
+            return Json(serialized, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> MakeReservation(MakeReservation makeReservation) {
+            var json = JsonSerializer<MakeReservation>.Serialize(makeReservation);
+            var jsonFromWebService = await ApiRequests.Post(ApiUrl.MAKE_RESERVATION, json);
+
+            return Json(jsonFromWebService, JsonRequestBehavior.AllowGet);
+        }
+
+        // Hashe epost + DateTime.now().ToString()
+        // Lagre det i customer db
+        // Blir invalidert etter x timer
+        // Adde hash som member i MakeReservation
+        // For å kunne foreta Reservasjon må man skjekke hashen mot databasen
+
+        [HttpPost]
+        [Route("api/ajax/signin/")]
+        public async Task<ActionResult> SignIn(Customer customer) {
+            var json = JsonSerializer<Customer>.Serialize(customer);
+            var jsonFromWebService = await ApiRequests.Post(ApiUrl.CUSTOMERS_EXIST, json);
+            return Json(jsonFromWebService, JsonRequestBehavior.DenyGet);
+        }
+
+        [HttpPost]
+        [Route("api/ajax/signup/")]
+        public async Task<ActionResult> SignUp(Customer customer)
+        {
+            var json = JsonSerializer<Customer>.Serialize(customer);
+            var jsonFromWebService = await ApiRequests.Post(ApiUrl.CUSTOMERS, json);    
+            return Json(jsonFromWebService, JsonRequestBehavior.DenyGet);
+        }
+    }
+}
