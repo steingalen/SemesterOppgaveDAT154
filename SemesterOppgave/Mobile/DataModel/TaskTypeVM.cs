048eebdb319573f43fab6cedca94cb53d5000c0c
Matmonsen <sindre.nerdal@gmail.com>
2016-03-02 03:16:46 +0100

    Added mobile

---
+++ SemesterOppgave/Mobile/DataModel/TaskTypeVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HttpRequest;
+using Models;
+
+namespace Mobile.DataModel
+{
+    class TaskTypeVM
+    {
+        public TaskTypeVM() {
+            Items = new ObservableCollection<TaskType>();
+        }
+
+        public ObservableCollection<TaskType> Items  { get; private set; }
+
+        public async Task Populate() {
+            var tasks = JsonSerializer<TaskType>.DeSerializeAsList(await ApiRequests.Get(ApiUrl.TASKTYPES));
+            Items = new ObservableCollection<TaskType>(tasks);
+        }
+
+
+    }
+}
---
+++ SemesterOppgave/Mobile/RoomTaskVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HttpRequest;
+using Models;
+
+namespace Mobile
+{
+    class RoomTaskVM
+    {
+        public RoomTaskVM(int taskType)
+        {
+            Items = new ObservableCollection<RoomTask>();
+            _taskType = taskType;
+        }
+
+        public ObservableCollection<RoomTask> Items { get; private set; }
+        private int _taskType;
+
+        public async Task Populate()
+        {
+            var tasks = JsonSerializer<RoomTask>.DeSerializeAsList(await ApiRequests.Get(ApiUrl.ROOM_TASKS_BY_TASK, _taskType));
+            Items = new ObservableCollection<RoomTask>(tasks);
+        }
+
+    }
+}
